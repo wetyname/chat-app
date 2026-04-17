b662ff611c9aadf31dedd343996b82a7394ec974
wetyname <wetyname@gmail.com>
2026-04-17 15:02:04 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -9,13 +9,15 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 DATA_FILE = 'users_data.json'
 online_users = 0
-muted_users = {} # Список тих, хто не може писати
+muted_users = {}
 
 def load_users():
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE, 'r', encoding='utf-8') as f:
-            try: return json.load(f)
-            except: return {}
+            try:
+                return json.load(f)
+            except:
+                return {}
     return {}
 
 def save_users(users):
@@ -40,59 +42,50 @@ def disconnect():
 @socketio.on('register_or_login')
 def handle_auth(data):
     users = load_users()
-    n, p, e = data.get('nick'), data.get('pass'), data.get('email')
+    n, p = data.get('nick'), data.get('pass')
     
+    if not n or not p: return
+
     if n in users and users[n].get('banned'):
-        return emit('auth_error', {'msg': 'Ви забанені на цьому сервері!'})
-        
+        return emit('auth_error', {'msg': 'Ви забанені!'})
+
     display_name = "Костя Гончаров" if n == "adminkgv2015" else n
+    
     if n in users:
-        if users[n]['pass'] == p: emit('auth_success', {'nick': display_name, 'real_nick': n})
-        else: emit('auth_error', {'msg': 'Невірний пароль!'})
-    else:
-        if not e: emit('need_email')
-        else:
-            users[n] = {'pass': p, 'email': e, 'banned': False}
-            save_users(users)
+        if users[n]['pass'] == p:
             emit('auth_success', {'nick': display_name, 'real_nick': n})
+        else:
+            emit('auth_error', {'msg': 'Невірний пароль!'})
+    else:
+        # Реєстрація нового користувача без вимоги email
+        users[n] = {'pass': p, 'email': "", 'banned': False}
+        save_users(users)
+        emit('auth_success', {'nick': display_name, 'real_nick': n})
 
 @socketio.on('message')
 def handle_msg(data):
-    user = data.get('username')
-    real_n = data.get('real_nick') # Використовуємо реальний нік для перевірок
-    
-    # Перевірка на мут
+    real_n = data.get('real_nick')
     if real_n in muted_users:
-        return emit('message', {'username': 'Система', 'message': 'Ви не можете писати, у вас мут!', 'time': ''})
+        return emit('message', {'username': 'Система', 'message': 'У вас мут!'})
 
     data['time'] = datetime.now().strftime("%H:%M")
     emit('message', data, broadcast=True)
 
-# --- АДМІН КОМАНДИ ---
 @socketio.on('admin_cmd')
 def admin_cmd(data):
     if data.get('admin') != "adminkgv2015": return
-    
-    cmd = data.get('cmd').split() # Розбиваємо команду на частини
+    cmd = data.get('cmd').split()
     users = load_users()
-    action = cmd[0] # /ban, /mute, /tban
+    action = cmd[0]
     target = cmd[1] if len(cmd) > 1 else ""
 
     if action == "/ban" and target in users:
         users[target]['banned'] = True
         save_users(users)
-        emit('message', {'username': 'Система', 'message': f'Користувач {target} забанений назавжди!'}, broadcast=True)
-        
+        emit('message', {'username': 'Система', 'message': f'{target} забанений!'}, broadcast=True)
     elif action == "/mute" and target:
         muted_users[target] = True
-        emit('message', {'username': 'Система', 'message': f'Користувачу {target} заборонено писати!'}, broadcast=True)
-
-    elif action == "/tban" and len(cmd) > 2:
-        sec = int(cmd[2])
-        muted_users[target] = True
-        emit('message', {'username': 'Система', 'message': f'{target} замучений на {sec} сек!'}, broadcast=True)
-        # Таймер розблокування
-        threading.Timer(sec, lambda: muted_users.pop(target, None)).start()
+        emit('message', {'username': 'Система', 'message': f'{target} отримав мут!'}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), allow_unsafe_werkzeug=True)
