3de4fa5f4336b25b73cf652a805f955217ec64e5
wetyname <wetyname@gmail.com>
2026-04-18 18:13:07 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,76 +1,77 @@
-import os, json, cloudinary, cloudinary.uploader
+import os, json, cloudinary, cloudinary.uploader, cloudinary.api
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'gkv_chat_777'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Налаштування Cloudinary
-cloudinary.config(
+# --- КОНФІГУРАЦІЯ ТРЬОХ АКАУНТІВ ---
+
+# 1. Для Фото
+config_photo = {
     cloud_name="dyssbgmjc",
     api_key="293523273862564",
     api_secret="HgCd_daKzEnkavFD-PCC2ZrHwrs"
-)
+}
 
-DATA_FILE = 'users_data.json'
+# 2. Для Відео (до 1 хв)
+config_video = {
+    "cloud_name": "NICK_VIDEO",
+    "api_key": "KEY_VIDEO",
+    "api_secret": "SECRET_VIDEO"
+}
 
-def load_users():
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, 'r', encoding='utf-8') as f:
-            try: return json.load(f)
-            except: return {}
-    return {}
+# 3. Для Кружечків (до 30-60 сек)
+config_circle = {
+    "cloud_name": "NICK_CIRCLE",
+    "api_key": "KEY_CIRCLE",
+    "api_secret": "SECRET_CIRCLE"
+}
 
-def save_users(users):
-    with open(DATA_FILE, 'w', encoding='utf-8') as f:
-        json.dump(users, f, ensure_ascii=False, indent=4)
+# Функція для швидкого очищення (видаляє найстаріші 20 файлів)
+def clean_if_full(conf, res_type):
+    try:
+        # Підключаємось до потрібного акаунта перед перевіркою
+        cloudinary.config(**conf)
+        res = cloudinary.api.resources(resource_type=res_type, max_results=500)
+        # Якщо файлів більше 100 (це приблизно і є межа безпеки для пам'яті)
+        if len(res['resources']) > 100:
+            to_del = [r['public_id'] for r in res['resources'][-20:]]
+            cloudinary.api.delete_resources(to_del, resource_type=res_type)
+    except: pass
 
-@app.route('/')
-def index(): return render_template('index.html')
-
-@socketio.on('register_or_login')
-def handle_auth(data):
-    users = load_users()
-    n = data.get('nick')
-    p = data.get('pass')
-    e = data.get('email') # Поле для пошти зберігається тут
+@socketio.on('send_media')
+def handle_media(data):
+    m_type = data['type']
+    file_to_up = data['file']
     
-    if not n or not p: return
+    if m_type == 'image':
+        curr_conf = config_photo
+        res_type = "image"
+        options = {}
+    elif m_type == 'video':
+        curr_conf = config_video
+        res_type = "video"
+        options = {"resource_type": "video"}
+    else: # circle
+        curr_conf = config_circle
+        res_type = "video"
+        options = {
+            "resource_type": "video",
+            "transformation": [{"width": 400, "height": 400, "crop": "fill", "gravity": "center"}, {"radius": "max"}]
+        }
 
-    display_name = "Костя Гончаров" if n == "adminkgv2015" else n
-
-    if n in users:
-        if users[n]['pass'] == p:
-            ava = users[n].get('avatar', 'https://cdn-icons-png.flaticon.com/512/149/149071.png')
-            emit('auth_success', {'nick': display_name, 'real_nick': n, 'avatar': ava})
-        else:
-            emit('auth_error', {'msg': 'Невірний пароль!'})
-    else:
-        # Реєстрація нового користувача з поштою та стандартною аватаркою
-        users[n] = {'pass': p, 'email': e, 'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
-        save_users(users)
-        emit('auth_success', {'nick': display_name, 'real_nick': n, 'avatar': users[n]['avatar']})
-
-@socketio.on('update_avatar')
-def update_avatar(data):
-    users = load_users()
-    real_n = data.get('real_nick')
-    if real_n in users:
-        up = cloudinary.uploader.upload(data.get('avatar'))
-        url = up['secure_url']
-        users[real_n]['avatar'] = url
-        save_users(users)
-        emit('avatar_changed', {'avatar': url, 'real_nick': real_n}, broadcast=True)
-
-@socketio.on('message')
-def handle_msg(data):
-    emit('message', data, broadcast=True)
-
-@socketio.on('delete_message')
-def handle_del(data):
-    emit('remove_msg', data, broadcast=True)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    # Очищуємо старе перед завантаженням нового
+    clean_if_full(curr_conf, res_type)
+    
+    # Завантажуємо на обраний акаунт
+    cloudinary.config(**curr_conf)
+    up = cloudinary.uploader.upload(file_to_up, **options)
+    
+    emit('message', {
+        'username': data['username'],
+        'type': m_type,
+        'url': up['secure_url'],
+        'avatar': data['avatar'],
+        'msg_id': "m-" + os.urandom(3).hex()
+    }, broadcast=True)
