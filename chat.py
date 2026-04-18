5745c468dab98337f3f9af97113f2c9911a71e1c
wetyname <wetyname@gmail.com>
2026-04-18 16:33:48 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,5 +1,12 @@
+Звісно! Ось оновлений код для обох файлів. Я додав логіку, яка зберігає посилання на аватарку в твій JSON-файл після завантаження в Cloudinary.
+
+Тепер, коли ти один раз зміниш фото, воно залишиться закріпленим за твоїм нікнеймом.
+
+1. Файл chat.py (Серверна частина)
+У цьому файлі я оновив функцію update_avatar, щоб вона зберігала посилання в базу, та переконався, що при вході (handle_auth) аватарка дістається з пам'яті.
+
+Python
 import os, json, cloudinary, cloudinary.uploader
-from datetime import datetime
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -7,11 +14,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gkv_chat_777'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# ВСТАВ СВОЇ ДАНІ ТУТ
-cloudinary.config( 
- cloud_name = "dyssbgmjc", 
-  api_key = "293523273862564", 
-  api_secret = "HgCd_daKzEnkavFD-PCC2ZrHwrs"
+# Налаштування Cloudinary
+cloudinary.config(
+    cloud_name="dyssbgmjc",
+    api_key="293523273862564",
+    api_secret="HgCd_daKzEnkavFD-PCC2ZrHwrs"
 )
 
 DATA_FILE = 'users_data.json'
@@ -19,8 +26,10 @@ DATA_FILE = 'users_data.json'
 def load_users():
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE, 'r', encoding='utf-8') as f:
-            try: return json.load(f)
-            except: return {}
+            try:
+                return json.load(f)
+            except:
+                return {}
     return {}
 
 def save_users(users):
@@ -35,37 +44,49 @@ def handle_auth(data):
     users = load_users()
     n, p = data.get('nick'), data.get('pass')
     if not n or not p: return
-    
+
     display_name = "Костя Гончаров" if n == "adminkgv2015" else n
-    
+
     if n in users:
         if users[n]['pass'] == p:
+            # Беремо аватарку з бази або ставимо стандартну
             ava = users[n].get('avatar', 'https://cdn-icons-png.flaticon.com/512/149/149071.png')
             emit('auth_success', {'nick': display_name, 'real_nick': n, 'avatar': ava})
         else:
             emit('auth_error', {'msg': 'Невірний пароль!'})
     else:
-        users[n] = {'pass': p, 'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
+        # Реєстрація нового користувача зі стандартною аватаркою
+        default_ava = 'https://cdn-icons-png.flaticon.com/512/149/149071.png'
+        users[n] = {'pass': p, 'avatar': default_ava}
         save_users(users)
-        emit('auth_success', {'nick': display_name, 'real_nick': n, 'avatar': users[n]['avatar']})
+        emit('auth_success', {'nick': display_name, 'real_nick': n, 'avatar': default_ava})
 
 @socketio.on('update_avatar')
 def update_avatar(data):
     users = load_users()
     real_n = data.get('real_nick')
-    if real_n in users:
-        # Відправляємо фото в хмару
-        up = cloudinary.uploader.upload(data.get('avatar'))
-        url = up['secure_url']
-        users[real_n]['avatar'] = url
+    image_data = data.get('avatar')
+
+    if real_n in users and image_data:
+        # 1. Завантажуємо в Cloudinary
+        upload_result = cloudinary.uploader.upload(image_data)
+        new_url = upload_result['secure_url']
+        
+        # 2. ЗБЕРЕЖЕННЯ В JSON (тепер вона не зникне)
+        users[real_n]['avatar'] = new_url
         save_users(users)
-        emit('avatar_changed', {'avatar': url})
+        
+        # 3. Повідомляємо всім про зміну
+        emit('avatar_changed', {'avatar': new_url, 'real_nick': real_n}, broadcast=True)
 
 @socketio.on('message')
 def handle_msg(data):
     emit('message', data, broadcast=True)
 
+@socketio.on('delete_message')
+def handle_del(data):
+    emit('remove_msg', data, broadcast=True)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # allow_unsafe_werkzeug=True ВИПРАВЛЯЄ ПОМИЛКУ З СКРІНШОТА
     socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
