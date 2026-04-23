0bca683f31c69f3646855cb924ce59666f6e9335
wetyname <wetyname@gmail.com>
2026-04-23 17:29:09 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,29 +2,19 @@ import eventlet
 eventlet.monkey_patch()
 
 import os, datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import cloudinary
-import cloudinary.uploader
-from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key_123'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+app.config['SECRET_KEY'] = 'secret'
+# Збільшуємо ліміт розміру повідомлення, щоб фото пролазили без хмари
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', max_http_buffer_size=10 * 1024 * 1024)
 
-# 1. CLOUDINARY (Встав свої дані)
-cloudinary.config(cloud_name="NAME", api_key="KEY", api_secret="SECRET", secure=True)
-
-# 2. MONGODB (Встав своє посилання)
-MONGO_URL = "mongodb+srv://..." 
-client = MongoClient(MONGO_URL)
-db = client['chat_db']
-messages_col = db['messages']
-
-# 3. СПИСОК АДМІНІВ (Зміни на свої)
+# Список адмінів
 ADMINS = {
-    "admin_yarik": "1111",
-    "vlad_boss": "2222"
+    "adminkgv2015": "777555111a?",
+    "Tri_paloski": "999000444555a?"
+    "lubly_$isi": "12130148q" 
 }
 
 @app.route('/')
@@ -42,26 +32,14 @@ def handle_auth(data):
         if ADMINS[nick] == password:
             emit('auth_success', {'nick': nick, 'role': 'admin'})
         else:
-            emit('auth_error', {'msg': 'Невірний пароль адміна!'})
+            emit('auth_error', {'msg': 'Невірний пароль!'})
     else:
-        emit('auth_error', {'msg': 'Можна зайти тільки як Анонім або Адмін'})
-
-    # Надсилаємо історію
-    history = list(messages_col.find().sort('_id', 1).limit(50))
-    for msg in history:
-        msg['_id'] = str(msg['_id'])
-        emit('message', msg)
+        emit('auth_error', {'msg': 'Тільки Анонім або Адмін'})
 
 @socketio.on('message')
 def handle_msg(data):
     data['time'] = datetime.datetime.now().strftime("%H:%M")
-    
-    if data.get('file') and data.get('type') == 'image':
-        res = cloudinary.uploader.upload(data['file'])
-        data['file'] = res['secure_url']
-
-    messages_col.insert_one(data.copy())
-    if '_id' in data: del data['_id']
+    # Просто пересилаємо дані всім, нікуди не зберігаючи
     emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
